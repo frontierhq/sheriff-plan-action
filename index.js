82cc7f5f24990eab2a6e0bd2f371eba9e4df730e
Craig Anderson <1877372+CDA0@users.noreply.github.com>
2024-03-22 14:56:02 +0000

    initial work

---
+++ index.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+async function run() {
+  try {
+    const configDir = core.getInput('configDir');
+    const mode = core.getInput('mode');
+
+    await tl.execAsync(
+      'sheriff',
+      [
+        'plan',
+        mode,
+        configDir ? '--config-dir' : '',
+        configDir,
+        '--subscription-id',
+        subscriptionId,
+      ],
+      {
+        env: {
+          ...process.env,
+          ...env,
+        },
+        silent: false,
+      },
+    );
+
+  } catch (err) {
+    if (err instanceof Error) {
+      core.setFailed(err.message);
+    } else {
+      core.setFailed('Unknown error');
+    }
+  }
+}
+
+run();
